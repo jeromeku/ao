0af98f0f753ef50d0d6ca383a438aa04153c3816
jeromeku <jerome.ku@gmail.com>
2025-02-09 04:22:06 -0800

    add argparser

--- my_tests/ddp-test.py
+++ my_tests/ddp-test.py
@@ -1,3 +1,4 @@
+import argparse
 import math
 import os
 import time
@@ -10,8 +11,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 from torchao.dtypes.nf4tensor import NF4Tensor, linear_nf4, to_nf4
 
-NUM_LINEARS = 1
-SEED = 42
+
 class LoRALinear(nn.Module):
     def __init__(
         self,
@@ -38,17 +38,16 @@ class LoRALinear(nn.Module):
         nn.init.kaiming_uniform_(self.lora_b.weight, a=math.sqrt(5))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        torch.manual_seed(SEED)
         out = linear_nf4(input=x, weight=self.weight)
         lora_out = self.lora_a(x)
         lora_out = (self.lora_alpha / self.lora_rank) * self.lora_b(lora_out)
         return out + lora_out
 
-def _init_model(dim=128, device="cuda", dtype=torch.float32) -> nn.Module:
+def _init_model(dim, num_linears, device, dtype) -> nn.Module:
     with torch.device(device):
 
         modules = []
-        for i in range(NUM_LINEARS):
+        for i in range(num_linears):
             modules += [LoRALinear(hidden_dim=dim, dtype=dtype)]
         seq = nn.Sequential(*modules)
 
@@ -70,21 +69,21 @@ def _print_params_and_grads(model, prefix=""):
         else:
             dist_print(f"{prefix}::DEBUG::PARAM", name, type(param), param.sum().item(), "None")
 
-def make_batch(bs, dim, dtype, device):
-    batch = torch.randn((bs, dim), dtype=dtype, device=device)
+def make_batch(global_bs, dim, dtype, device):
+    batch = torch.randn((global_bs, dim), dtype=dtype, device=device)
     if dist.get_world_size() > 1:
         batch = batch.chunk(dist.get_world_size(), dim=0)[dist.get_rank()]
     return batch
 
-def test_ddp(bs=2, dim=128, num_steps=3, device="cuda", dtype=torch.float32):
-    model = _init_model(dim, device, dtype)
+def test_ddp(global_bs, dim, num_linears, device, dtype, num_steps):
+    model = _init_model(dim, num_linears, device, dtype)
     model = DDP(model, device_ids=[device])
     optim = torch.optim.Adam(model.parameters(), lr=1e-2)
     
     losses = []
     
     for i in range(num_steps):
-        inp = make_batch(bs, dim, dtype, device)
+        inp = make_batch(global_bs, dim, dtype, device)
         loss = model(inp).sum()
         losses.append(loss)
         #dist_print(f"LOSS::STEP_{i}", loss.item())
@@ -127,6 +126,17 @@ def distributed_context():
     cleanup_dist()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--global_bs", type=int, default=8)
+    parser.add_argument("--dim", type=int, default=128)
+    parser.add_argument("--num_linears", type=int, default=1)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--dtype", type=str, default="float32")
+    parser.add_argument("--num_steps", type=int, default=3)
+    args = parser.parse_args()
+
     with distributed_context():
-        torch.manual_seed(SEED)
-        test_ddp()
+        torch.manual_seed(args.seed)
+        test_ddp(args.global_bs, args.dim, args.num_linears, args.device, args.dtype, args.num_steps)
