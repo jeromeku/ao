1991d9890ea8a50abf2b8fc89e5394c077e4e7b6
jeromeku <jerome.ku@gmail.com>
2025-02-24 19:14:56 -0800

    update lut

--- scratch/lut.py
+++ scratch/lut.py
@@ -12,15 +12,16 @@ def _lut(x):
 def lut_kernel(x_ptr, y_ptr, N: tl.constexpr):
     load_idx = tl.arange(0, N)
     x = tl.load(x_ptr + load_idx)
-    y = tl.where(x > 0, tl.where(x > 1, tl.where(x > 2, 4, 5), 3), 2)
+    y = tl.where(x < 1, -2.0, tl.where(x < 2, -1.0, tl.where(x < 3, 0.0, 1.0)))
     tl.store(y_ptr + load_idx, y)
 
 
 x = torch.arange(0, 8, dtype=torch.uint8, device="cuda")
-y = torch.empty_like(x, dtype=torch.uint8, device="cuda")
+N = x.numel()
+y = torch.zeros(N, dtype=torch.float32, device="cuda")
 
 grid = (1,)
-N = x.numel()
+
 print(x)
 lut_kernel[grid](x, y, N)
 
